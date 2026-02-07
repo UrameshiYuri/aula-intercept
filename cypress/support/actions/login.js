3e0a4b0f202df9d46f00160ae684d8ff4abaefc1
Yuri Mielli <yumielllli@gmail.com>
2026-02-07 12:11:48 +0100

    correçao de erros ortograficos

--- cypress/support/actions/login.js
+++ cypress/support/actions/login.js
@@ -4,6 +4,7 @@ import { loginPage } from '../pages'
 Cypress.Commands.add('login', (email, senha) => {
     cy.setCookie('ebacStoreVersion', 'v2', { domain: 'lojaebac.ebaconline.art.br' })
     cy.visit("/")
+    cy.wait(10000)
     homePage.openMenu('Account')
     loginPage.login(email, senha)
     //homePage.openMenu('Account')
